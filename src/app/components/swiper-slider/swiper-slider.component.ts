04e174756d51738cecd0f35463a6a1c47c1fdc0f
Reza Rahimi <R.Rahimi@aprofile.ir>
2024-06-15 12:18:01 +0330

    s

--- src/app/components/swiper-slider/swiper-slider.component.ts
+++ src/app/components/swiper-slider/swiper-slider.component.ts
@@ -7,13 +7,15 @@ import {
 } from '@angular/core';
 import Swiper from 'swiper';
 import 'swiper/css';
-import { Navigation } from 'swiper/modules';
 import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper/modules';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-swiper-slider',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './swiper-slider.component.html',
   styleUrl: './swiper-slider.component.scss',
@@ -23,19 +25,33 @@ export class SwiperSliderComponent implements OnInit {
     required: true,
   })
   images!: string[];
-
   swiper!: Swiper;
 
   ngOnInit(): void {
     this.swiper = new Swiper('.mySwiper', {
       direction: 'horizontal',
-      speed: 600,
+      speed: 300,
+      spaceBetween: 5,
+      grabCursor: true,
       loop: true,
+      // autoplay: {
+      //   delay: 5000,
+      // },
       navigation: {
         nextEl: '.next-btn',
         prevEl: '.prev-btn',
       },
-      modules: [Navigation],
+      pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+        clickable: true,
+        // dynamicMainBullets: true,
+      },
+      modules: [Navigation, Pagination],
     });
+  }
+
+  slideTo(index: number) {
+    this.swiper.slideTo(index);
   }
 }
